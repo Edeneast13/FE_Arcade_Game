3ac3b330c9b7d178ac75fabddad59d2f45d2dd1f
Brian Roper <broper138@gmail.com>
2017-04-07 14:06:45 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,95 @@
+var Enemy = function(x, y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+};
+
+Enemy.prototype.update = function(dt) {
+    this.x += this.speed * dt;
+
+    if(this.x > 500){
+        this.x = -50;
+        this.speed = 50 + Math.floor(Math.random() * 500);
+    }
+
+    if(player.x < this.x + 60 && player.x + 60 > this.x && player.y < this.y + 20 &&
+        20 + player.y > this.y){
+        player.y = 380;
+        player.x = 200;
+    }
+};
+
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+var Player = function(x, y, speed){
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = "images/char-boy.png";
+};
+
+Player.prototype.update =  function(){
+    if(player.x <= 0){
+        player.x = 0;
+    }
+    if(player.x >= 400){
+        player.x = 400;
+    }
+    if(player.y >= 400){
+        player.y = 400;
+    }
+    if(player.y <= -20){
+        player.y = 400;
+    }
+};
+
+Player.prototype.render = function(){
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+Player.prototype.handleInput = function(keyPress){
+    switch(keyPress){
+        case 'left':
+            player.x -= player.speed -10;
+            break;
+        
+        case 'right':
+            player.x += player.speed - 10;
+            break;
+        
+        case 'up':
+            player.y -= player.speed - 25;
+            break;
+        
+        case 'down':
+            player.y += player.speed - 25;
+            break;
+    }
+};  
+
+var allEnemies = [];
+
+var enemyPosition = [60, 140, 220];
+var enemy;
+var player = new Player(200, 400, 50);
+
+enemyPosition.forEach(function(y){
+    enemy = new Enemy(0, y, 100 + Math.floor(Math.random() * 500));
+    allEnemies.push(enemy);
+});
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method. You don't need to modify this.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
